05d9730d121277e8db79168016bd09de1857a37f
Vít Falta <falta.vit@zsbcuo.cz>
2020-05-10 22:19:25 +0200

    fixed error handling to convert message to string

--- src/config/variables.ts
+++ src/config/variables.ts
@@ -1,7 +1,7 @@
-export const YOUTUBE_API_KEY = "YOUR_API_KEY"
+export const YOUTUBE_API_KEY = process.env['YOUTUBE_API_KEY']
 
-export const WS_PORT = 8080
-export const HTTP_PORT = 8000
+export const WS_PORT = parseInt(process.env['WS_PORT']) || 8080
+export const HTTP_PORT = parseInt(process.env['HTTP_PORT']) || 8000
 
 export const CHANGE_PROPAGATION_TIMOUT = 1
 
