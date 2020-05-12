ef0a4c742412fc7010947c19a00b3cec33f0be91
Vít Falta <falta.vit@zsbcuo.cz>
2020-05-12 12:28:43 +0200

    improved computeScore function

--- src/helpers/compute-score.ts
+++ src/helpers/compute-score.ts
@@ -1,5 +1,5 @@
 import { Rating } from "../types/rating.type";
 
 export const computeScore = (rating: Rating[]) => {
-    return rating.reduce((prev, cur) => prev + cur.value, 0) / rating.filter(rating => rating.value !== 0).length
+    return rating.reduce((prev, cur) => prev + cur.value, 0)
 }
