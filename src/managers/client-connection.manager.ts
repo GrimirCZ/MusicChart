43725c3902597af47a5ea580c3e92753644f3a5c
Vít Falta <falta.vit@zsbcuo.cz>
2020-05-12 10:34:41 +0200

    added pings

--- src/managers/client-connection.manager.ts
+++ src/managers/client-connection.manager.ts
@@ -1,7 +1,5 @@
 import WebSocket = require('ws');
 import { User } from "../types/user.type";
-import { ConnectionData } from "../types/connection-data.type";
-import wss from "../config/ws.server";
 
 let connection = new WeakMap<WebSocket, User>()
 
