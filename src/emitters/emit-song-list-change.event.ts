9850f67c9a6a7e3753d4aa36f7113b7c0e014fcb
Vít Falta <falta.vit@zsbcuo.cz>
2020-05-13 10:26:31 +0200

    changed your rating for all ratings

--- src/emitters/emit-song-list-change.event.ts
+++ src/emitters/emit-song-list-change.event.ts
@@ -22,9 +22,12 @@ export const emitSongListChangeEvent = (ws: WebSocket, user: User) => {
             userId: user.id,
             userName: user.name,
             currentScore: computeScore(ratings),
-            yourRating: ratings.filter(rating => rating.user.id === user.id)[0].value,
 
-            dbgRatings: ratings.map(rating => [rating.value, rating.user.id, rating.user.name]),
+            ratings: ratings.map(rating => ({
+                userId: rating.user.id,
+                userName: rating.user.name,
+                rating: rating.value
+            })),
 
             hasPlayed
         }))
